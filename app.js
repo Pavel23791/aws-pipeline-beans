96c5efa14c91e277e12d254e429a5e72c9b32e1a
Pavel23791 <140964820+Pavel23791@users.noreply.github.com>
2023-09-20 19:48:14 +0300

    add message

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 app.use('/', (req, res) => {
-    return res.status(200).json({message: "Hello From AWS Pipline"});
+    return res.status(200).json({message: "Hello From AWS Pipline and beanstalk"});
 })
 
 app.listen(3001);
